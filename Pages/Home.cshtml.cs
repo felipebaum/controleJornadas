84c3760405225c37da924683fd4b4f32ae2e5ae4
felipe <felipe_1100@hotmail.com>
2023-12-08 12:36:56 -0300

    ok

--- Pages/Home.cshtml.cs
+++ Pages/Home.cshtml.cs
@@ -1,17 +1,31 @@
-using controleJornadas.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using controleJornadas.Models;
 
 namespace controleJornadas.Pages
 {
     public class HomeModel : PageModel
     {
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public HomeModel(SignInManager<Usuario> signInManager)
+        {
+            _signInManager = signInManager;
+        }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+
+            return RedirectToPage("/Index"); // Redirecionar para a página inicial ou outra página de sua escolha
         }
     }
 }
